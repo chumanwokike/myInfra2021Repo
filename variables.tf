aeaa452137774c69c8323d7f96124ab0b0652070
chumanwokike <112478381+chumanwokike@users.noreply.github.com>
2023-03-08 22:27:23 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "aws_region" {
 
 variable "key_name" { 
     description = " SSH keys to connect to ec2 instance" 
-    default     =  "myJenkinsKey" 
+    default     =  "mySep22Key" 
 }
 
 variable "instance_type" { 
